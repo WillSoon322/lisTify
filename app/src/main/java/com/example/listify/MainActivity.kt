947347c11c36afecbbba2a477e055f3fcae3e9df
Kurni <kurniawandimas519@yahoo.com>
2021-10-15 22:35:28 +0700

    1

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.listify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.listify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var x:H
+
+    private lateinit var  binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    var x : H
-    x.angka
+
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.setSupportActionBar(binding.toolbar)
+
+
+        var ab :ActionBarDrawerToggle
+        ab= ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolbar,R.string.Open,R.string.Close)
+        binding.drawerLayout.addDrawerListener(ab)
+        ab.syncState()
+
+
+
+
+   // x.angka
 
     }
 
