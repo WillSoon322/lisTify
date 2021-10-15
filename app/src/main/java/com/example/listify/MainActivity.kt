395e1d7385209868beba811d0d745e3c4164071d
flakyriemaid <valkkyriemaid@gmail.com>
2021-10-15 13:32:47 +0700

    add menu image

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var x:H
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    var x : H
-    x.angka
-
+        x.angka
     }
 
 
